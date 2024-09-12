6e4020372bd9ae9d2d7eaceee3057f6ad4039ba3
Byounghern Kim <be.honey@si-analytics.ai>
2024-09-12 21:11:34 +0900

    chore: hydrate client, metadata

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -1,4 +1,4 @@
-import { api } from "@/trpc/server";
+import { HydrateClient, api } from "@/trpc/server";
 import OurTodoList from "../_components/OurTodoList";
 import type { TodoWithUser } from "../_types/todo";
 
@@ -8,9 +8,11 @@ export default async function OtherPage() {
   const initialTodos = await api.todo.getWhole() as TodoWithUser[];
 
   return (
+    <HydrateClient>
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">{"Our Todo"}</h1>
       <OurTodoList initialTodos={initialTodos} />
     </div>
+    </HydrateClient>
   );
 }
--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,7 +1,7 @@
 import { TodoList } from "@/app/_components/TodoList";
 import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
-import { type Todo } from "@prisma/client";
+import { HydrateClient, api } from "@/trpc/server";
+import type { Todo } from "@prisma/client";
 
 export default async function TodoPage() {
   const session = await getServerAuthSession();
@@ -22,11 +22,13 @@ export default async function TodoPage() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight">Todo List</h1>
-        <TodoList initialTodos={todos} />
-      </div>
-    </main>
+          <h1 className="text-5xl font-extrabold tracking-tight">Todo List</h1>
+          <TodoList initialTodos={todos} />
+        </div>
+      </main>
+    </HydrateClient>
   );
 }
