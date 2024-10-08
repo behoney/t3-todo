4d43e2175d190cb260a3b5bd92d1a578cf43c6d0
Byounghern Kim <be.honey@si-analytics.ai>
2024-10-08 21:33:35 +0900

    chore: move refetching logic to query-client

--- src/app/_components/OurTodoList.tsx
+++ src/app/_components/OurTodoList.tsx
@@ -1,41 +1,29 @@
 "use client"
 
 import { api } from "@/trpc/react";
-import { useEffect } from "react";
 import type { TodoWithUser } from "../_types/todo";
 
-
 export default function OurTodoList({
-  initialTodos
+  initialTodos,
 }: {
-    initialTodos: readonly TodoWithUser[]
+  initialTodos: readonly TodoWithUser[];
 }) {
   const todoQuery = api.todo.getWhole.useQuery();
   const todos = todoQuery.data ?? initialTodos;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      void todoQuery.refetch();
-    }, 15000);
-    return () => clearInterval(interval);
-  }, [])
-
   return (
-
-    <div className="flex flex-col gap-4 rounded-md bg-white p-4 w-full max-w-md">
+    <div className="flex w-full max-w-md flex-col gap-4 rounded-md bg-white p-4">
       {todos.map((todo) => {
         return (
-
           <div
             key={todo.id}
-            className={`rounded-md bg-gray-100 px-4 py-2 text-black w-full
-        ${todoQuery.isRefetching ? "bg-gray-200" : ""}`}
+            className={`w-full rounded-md bg-gray-100 px-4 py-2 text-black ${todoQuery.isRefetching ? "bg-gray-200" : ""}`}
           >
             <p className="text-lg font-bold">{todo.text}</p>
-            <desc className="text-sm w-full text-right text-gray-500">{`By ${todo.user.name} @ ${todo.createdAt.toLocaleDateString()}`}</desc>
+            <desc className="w-full text-right text-sm text-gray-500">{`By ${todo.user.name} @ ${todo.createdAt.toLocaleDateString()}`}</desc>
           </div>
         );
       })}
     </div>
-  )
+  );
 }
--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -8,9 +8,8 @@ export const createQueryClient = () =>
   new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        staleTime: 15 * 1000,
+        refetchInterval: 15 * 1000,
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
